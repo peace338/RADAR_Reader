ed87c44ffe1d1ea8273070332ad5edd6add8e63d
peace338 <peace338@gmail.com>
2023-08-28 13:13:08 +0900

    max_z, min_z is redeifned.

--- radarEquipInfo.py
+++ radarEquipInfo.py
@@ -17,3 +17,6 @@ And removed object will not be used as the input of algorithm emulator.
 AZIM_FOV = 130 
 ELEV_FOV = 80 
 MAX_HEIGHT = 3.0 
+
+MAX_Z = MAX_HEIGHT - EQUIP_HEIGHT
+MIN_Z = -EQUIP_HEIGHT
